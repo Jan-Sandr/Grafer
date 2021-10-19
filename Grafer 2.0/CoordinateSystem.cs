5443dd44345f7413efd8e015ff61f2c8eb9184ac
Jan Sandr <honzasandr@seznam.cz>
2021-10-19 15:01:56 +0200

    #46 Implemented Method which draw grid numbers

--- Grafer 2.0/CoordinateSystem.cs
+++ Grafer 2.0/CoordinateSystem.cs
@@ -23,6 +23,7 @@ namespace Grafer2
         {
             DrawAxes();
             DrawGrid();
+            DrawNumbers();
         }
 
         private void DrawGrid()
@@ -31,23 +32,71 @@ namespace Grafer2
             DrawGridLines(Direction.Y, Height);
         }
 
-        private void DrawGridLines(Direction direction, double size)
+        private void DrawNumbers()
         {
-            for (int i = 100; i < size; i += 100)
+            DrawGridNumbers(Direction.X, Width);
+            DrawGridNumbers(Direction.Y, Height);
+        }
+
+        private void DrawGridNumbers(Direction direction, double size)
+        {
+            for (int i = 100; i < size / 2; i += 100)
             {
-                Line gridLine = new();
-                
                 switch (direction)
                 {
                     case Direction.X:
                         {
-                            gridLine = NewLine(direction, lineY: i);
+                            Children.Add(NewNumber(-i / 100, size / 2 - i - 12, Height / 2 + 10));
+                            Children.Add(NewNumber(i / 100, size / 2 + i - 5, Height / 2 + 10));
                             break;
                         }
 
                     case Direction.Y:
                         {
-                            gridLine = NewLine(direction, lineX: i);
+                            Children.Add(NewNumber(i / 100, Width / 2 + 16, size / 2 - i - 15));
+                            Children.Add(NewNumber(-i / 100, Width / 2 + 10, size / 2 + i - 15));
+                            break;
+                        }
+                }
+            }
+
+            Children.Add(NewNumber(0, Width / 2 + 16, Height / 2 + 10));
+        }
+
+        private TextBlock NewNumber(int value, double x, double y)
+        {
+            TextBlock number = new();
+            number.Text = value.ToString();
+            number.FontSize = 20;
+
+            number.RenderTransform = new TranslateTransform()
+            {
+                X = x,
+                Y = y
+            };
+
+            return number;
+        }
+
+        private void DrawGridLines(Direction direction, double size)
+        {
+            Color color = Color.FromArgb(100, 0, 0, 0);
+
+            for (int i = 100; i < size; i += 100)
+            {
+                Line gridLine = new();
+                SolidColorBrush brush = new SolidColorBrush(color);
+                switch (direction)
+                {
+                    case Direction.X:
+                        {
+                            gridLine = NewLine(direction, lineY: i, brushes: brush);
+                            break;
+                        }
+
+                    case Direction.Y:
+                        {
+                            gridLine = NewLine(direction, lineX: i, brushes: brush);
                             break;
                         }
                 }
@@ -68,6 +117,7 @@ namespace Grafer2
 
         private Line NewLine(Direction direction, double lineX = 0, double lineY = 0, SolidColorBrush? brushes = null, double strokeThickness = 1)
         {
+
             Line line = new()
             {
                 X1 = direction == Direction.X ? 0 : lineX,
@@ -75,7 +125,7 @@ namespace Grafer2
                 X2 = direction == Direction.X ? Width : lineX,
                 Y2 = direction == Direction.X ? lineY : Height,
                 Stroke = brushes ?? Brushes.Black,
-                StrokeThickness = strokeThickness
+                StrokeThickness = strokeThickness,              
             };
 
             line.SnapsToDevicePixels = true;
