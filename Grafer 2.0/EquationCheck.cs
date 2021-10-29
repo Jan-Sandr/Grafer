6e975d93f9e937ae34054747b0b463d55bc40d6d
Jan Sandr <honzasandr@seznam.cz>
2021-10-29 10:54:09 +0200

    #64 New method AreTwoOperationsInRow

--- Grafer 2.0/EquationCheck.cs
+++ Grafer 2.0/EquationCheck.cs
@@ -1,12 +1,24 @@
-﻿namespace Grafer2
+﻿using System.Linq;
+
+namespace Grafer2
 {
     public static class EquationCheck
     {
         public static bool BasicCheck(Relation relation)
         {
-            bool isRelationValid = AreEdgesValid(relation);
+            bool isRelationValid;
+
+            isRelationValid = AreEdgesValid(relation);
+
+            if(isRelationValid)
+            {
+                isRelationValid = !AreTwoOperationsInRow(relation);
+            }
+            
             return isRelationValid;
         }
+
+        private static readonly string[] mathOpertions = new string[4] { "+", "-", "*", "/" };
 
         private static bool AreEdgesValid(Relation relation)
         {
@@ -22,6 +34,22 @@
             }
 
             return areEdgesValid;
+        }
+
+        private static bool AreTwoOperationsInRow(Relation relation)
+        {
+            bool areTwoOperationsInRow = false;
+
+            for (int i = 0; i < relation.Count-1; i++)
+            {
+                if(mathOpertions.Contains(relation[i]) && mathOpertions.Contains(relation[i+1]))
+                {
+                    areTwoOperationsInRow = true;
+                    break;
+                }
+            }
+
+            return areTwoOperationsInRow;
         }
     }
 }
