4b53649afeb97d372700fd5b6ffcd8a84ef31f68
Jan Sandr <honzasandr@seznam.cz>
2021-12-19 13:08:42 +0100

    Renamed project

---
+++ Grafer/Function.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Grafer
+{
+    public class Function
+    {
+        public Relation Relation { get; set; }
+        public double MinimumX { get; set; }
+        public double MaximumX { get; set; }
+        public CalculationOrder CalculationOrder { get; set; }
+        public List<Polyline> Curves { get; set; }
+        public Canvas Canvas { get; }
+
+        private List<Point> points;
+        private double y;
+        private string[] relationBackup = Array.Empty<string>(); // Záloha předpisu.
+        private int[] calculationOrderIndexesBackup = Array.Empty<int>(); // záloha výpočetního postupu.
+        private double calculationMinimumX, calculationMaximumX; // Výpočetní minimum a maximum.
+
+        public Function(string relation, double minimumX, double maximumX, Canvas canvas)
+        {
+            Relation = new Relation(relation);
+            MinimumX = minimumX;
+            MaximumX = maximumX;
+            CalculationOrder = new CalculationOrder();
+            Curves = new List<Polyline>();
+            points = new List<Point>();
+            Canvas = canvas;
+        }
+
+        //Výpočítání křivky.
+        public void CalculatePoints()
+        {
+            PrepareForCalculation();
+            DoCalculation();
+            SaveCurve(points);
+            points = new List<Point>();
+        }
+
+        //Výpoočet bodů.
+        private void DoCalculation()
+        {
+            for (double x = calculationMinimumX; x <= calculationMaximumX; x += 0.01)
+            {
+                GetBackup();
+
+                SetXInRelation(x);
+
+                ComputeY();
+
+                SavePoint(x, y);
+            }
+
+        }
+
+        //Nastevní aktuální hodnoty x v předpisu.
+        private void SetXInRelation(double x)
+        {
+            x = Math.Round(x, 2);
+
+            SubstituteX(x);
+        }
+
+        //Vypočet y.
+        private void ComputeY()
+        {
+            y = 0;
+
+            y = (Relation.Count > 1) ? CalculateYForX() : double.Parse(Relation[0]);
+        }
+
+        //Příprava pro výpoočet.
+        private void PrepareForCalculation()
+        {
+            CalculationOrder.Create(Relation);
+            SetBackup();
+            SetCalculationXRange();
+        }
+
+        //Omezení y pro případ velkých čísel.
+        private double LimitY()
+        {
+            if (!double.IsInfinity(y))
+            {
+                if (y > 1000000)
+                {
+                    y = 1000000;
+                }
+
+                if (y < -1000000)
+                {
+                    y = -1000000;
+                }
+            }
+
+            return y;
+        }
+
+        //Vykreslení funkce do plátna.
+        public void Plot()
+        {
+            for (int i = 0; i < Curves.Count; i++)
+            {
+                Canvas.Children.Add(Curves[i]);
+            }
+        }
+
+        //Nastavení výpočetního rozsahu. Pokud by byl rozsah větší než plátno, omezí to jen na viditelnou plochu interně.
+        private void SetCalculationXRange()
+        {
+            calculationMinimumX = (Math.Abs(MinimumX) > Canvas.Width / 200) ? -(Canvas.Width / 200) : MinimumX;
+            calculationMaximumX = (MaximumX > Canvas.Width / 200) ? Canvas.Width / 200 : MaximumX;
+        }
+
+        //Dosazení za x.
+        private void SubstituteX(double x)
+        {
+            for (int i = 0; i < Relation.Count; i++)
+            {
+                Relation[i] = Relation[i] == "x" ? x.ToString() : Relation[i];
+                Relation[i] = Relation[i] == "-x" ? (-x).ToString() : Relation[i];
+            }
+        }
+
+        //Výpočet y pro x.
+        private double CalculateYForX()
+        {
+            int orderProgression = 0;
+
+            while (Relation.Count > 1 && !double.IsNaN(y))
+            {
+                y = CalculateYValue(orderProgression);
+                orderProgression++;
+            }
+
+            y = Math.Abs(y) > 1000000 ? LimitY() : y;
+
+            return y;
+        }
+
+        //Výpočet hodnoty y.
+        private double CalculateYValue(int orderProgression)
+        {
+            int index = CalculationOrder.Indexes[orderProgression];
+
+            Relation[index] = Operation(index).ToString();
+
+            Relation.RemoveNeighbors(index);
+
+            CalculationOrder.ShiftPosition(Relation.RemovedElementsCount, orderProgression);
+
+            Relation.RemovedElementsCount = 0;
+
+            return y;
+        }
+
+        //Uložení bodu.
+        private void SavePoint(double x, double y)
+        {
+            if (!double.IsNaN(y) && !double.IsInfinity(y)) // Pokud bod není definovaný, vzniká mezera.
+            {
+                Point point = new Point(x, y);
+                point = ConvertToCoordinatePoint(point);
+                points.Add(point);
+            }
+            else if (points.Count > 0)
+            {
+                if (points.Count > 1) // Křivka minimálně ze 2 bodů.
+                {
+                    SaveCurve(points);
+                }
+
+                points = new List<Point>();
+            }
+        }
+
+        //Uložení křivky.
+        private void SaveCurve(List<Point> points)
+        {
+            Polyline polyline = new Polyline()
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+
+            polyline.SnapsToDevicePixels = true;
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                polyline.Points.Add(points[i]);
+            }
+
+            Curves.Add(polyline);
+        }
+
+        //Převedení bodu na bod do soustavy.
+        private Point ConvertToCoordinatePoint(Point point)
+        {
+            point = new Point()
+            {
+                X = Math.Round(Canvas.Width / 2 + point.X * 100, 2),
+                Y = (-y * 100) + Canvas.Height / 2
+            };
+            return point;
+        }
+
+        //Operace mezi 2 členy v předpisu.
+        private double Operation(int index)
+        {
+            switch (Relation[index])
+            {
+                case "+":
+                    {
+                        y = double.Parse(Relation[index - 1]) + double.Parse(Relation[index + 1]);
+                        break;
+                    }
+                case "-":
+                    {
+                        y = double.Parse(Relation[index - 1]) - double.Parse(Relation[index + 1]);
+                        break;
+                    }
+                case "*":
+                    {
+                        y = double.Parse(Relation[index - 1]) * double.Parse(Relation[index + 1]);
+                        break;
+                    }
+                case "/":
+                    {
+                        y = double.Parse(Relation[index - 1]) / double.Parse(Relation[index + 1]);
+                        break;
+                    }
+                case "^":
+                    {
+                        y = Math.Pow(double.Parse(Relation[index - 1]), double.Parse(Relation[index + 1]));
+                        break;
+                    }
+                case "√":
+                    {
+                        y = Root(double.Parse(Relation[index + 1]), 1 / double.Parse(Relation[index - 1]));
+                        break;
+                    }
+            }
+
+            return y;
+        }
+
+        //Odmocnění
+        private double Root(double number, double index)
+        {
+            return (index > 0 && !double.IsInfinity(index)) ? Math.Pow(number, index) : double.NaN;
+        }
+
+        //Vytvoření zálohy pro výpočet.
+        private void SetBackup()
+        {
+            relationBackup = Relation.ToArray();
+            calculationOrderIndexesBackup = new int[CalculationOrder.Indexes.Length];
+            CalculationOrder.Indexes.CopyTo(calculationOrderIndexesBackup, 0);
+        }
+
+        //Načtení zálohy.
+        private void GetBackup()
+        {
+            Relation = new Relation();
+            Relation.AddRange(relationBackup);
+            calculationOrderIndexesBackup.CopyTo(CalculationOrder.Indexes, 0);
+        }
+    }
+}
---
+++ Grafer/Relation.cs
@@ -0,0 +1,163 @@
+﻿using Grafer.ExtensionMethods;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Grafer
+{
+    public class Relation : List<string>
+    {
+        public int RemovedElementsCount { get; set; }
+
+        //Prázdný konstruktor.
+        public Relation()
+        {
+
+        }
+
+        //Naplnění předpisu.
+        public Relation(string input)
+        {
+            AddRange(input.Select(s => s.ToString()));
+            Adjust();
+        }
+
+        //Úprava předpisu pro výpočet.
+        private void Adjust()
+        {
+            RemoveAll(s => s == " ");
+
+            if (Count > 1)
+            {
+                Insertions();
+
+                ConnectNumbers();
+
+                RemoveUnnecessaryBrackets();
+            }
+        }
+
+        //Vkládání znaků dle potřeby pro standard.
+        private void Insertions()
+        {
+            InsertAtBeginning();
+
+            InsertMultiplication();
+
+            InsertRootIndex();
+
+            InsertZero();
+        }
+
+        //Odebrání zbytečných závorek x + (2).
+        private void RemoveUnnecessaryBrackets()
+        {
+            for (int i = 1; i < Count - 1; i++)
+            {
+                CheckNeighbors(i);
+            }
+        }
+
+        private void InsertAtBeginning()
+        {
+            if (this[0] == "-")
+            {
+                Insert(0, "0");
+            }
+
+            if (this[0] == "√")
+            {
+                Insert(0, "2");
+            }
+        }
+
+        //Vložení nulu. Když předpis začíná minusem a nebo (-.
+        private void InsertZero()
+        {
+            for (int i = 1; i < Count; i++)
+            {
+                if (this[i] == "-" && this[i - 1] == "(")
+                {
+                    Insert(i, "0");
+                    i++;
+                }
+            }
+        }
+
+        //Vložení znaku  pro násobení .
+        private void InsertMultiplication()
+        {
+            for (int i = 1; i < Count; i++)
+            {
+                if (CanInsertMultiplication(this[i - 1], this[i]))
+                {
+                    Insert(i, "*");
+                }
+            }
+        }
+
+        //Tabulka pro vložení znaku pro násobení.
+        private static bool CanInsertMultiplication(string left, string right)
+        {
+            return (
+                        (char.IsLetter(char.Parse(left)) && char.IsLetter(char.Parse(right))) ||
+                        (char.IsDigit(char.Parse(left)) && char.IsLetter(char.Parse(right))) ||
+                        (char.IsLetter(char.Parse(left)) && char.IsDigit(char.Parse(right))) ||
+                        (char.IsLetterOrDigit(char.Parse(left)) && right == "(") ||
+                        (left == ")" && char.IsLetterOrDigit(char.Parse(right))) ||
+                        (left == ")" && right == "(")
+                   );
+        }
+
+        //Spojení čísel do políčka třeba 10 je v základu jako 1 a 0. 
+        private void ConnectNumbers()
+        {
+            for (int i = 1; i < Count; i++)
+            {
+                if (double.TryParse(this[i], out _) && double.TryParse(this[i - 1], out _))
+                {
+                    this[i - 1] += this[i];
+                    RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+        //Pokud uživatel napsal jenom odmocninu převede to na 2. odmocninu, protože ta se dá napsat bez indexu.
+        private void InsertRootIndex()
+        {
+            for (int i = 1; i < Count; i++)
+            {
+                if (this[i] == "√" && (this[i - 1].IsMathOperation() || this[i - 1] == "("))
+                {
+                    Insert(i, "2");
+                    i++;
+                }
+            }
+        }
+
+        //Odebrání sousedů elementu.
+        public void RemoveNeighbors(int index)
+        {
+            RemoveAt(index + 1);
+            RemoveAt(index - 1);
+
+            index--;
+
+            RemovedElementsCount += 2;
+
+            CheckNeighbors(index);
+        }
+
+        //Zda jsou sousedé závorky.
+        private void CheckNeighbors(int index)
+        {
+            if (index != 0 && index < Count - 1)
+            {
+                if (this[index - 1] == "(" && this[index + 1] == ")")
+                {
+                    RemoveNeighbors(index);
+                }
+            }
+        }
+    }
+}
